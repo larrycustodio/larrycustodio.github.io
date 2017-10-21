5b9d6ab98389b527846d03b48ed52aab499024f8
Larry Custodio <larrycustodio@Larrys-MacBook-Pro.local>
2017-10-20 21:17:22 -0700

    navbar edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,24 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
 const nodemon = require('gulp-nodemon');
 const browserSync = require('browser-sync').create();
 
 gulp.task('sass', () => {
-    return gulp.src('./assets/scss/**/*.scss')
-        .pipe(sass('error',sass.logError))
+    return gulp.src('assets/scss/**/*.scss')
+        .pipe(sass())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('/assets/css'))
+        .pipe(gulp.dest('assets/css'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('css', () => {
-    return gulp.src('./assets/css/**/*.css')
+    return gulp.src('assets/css/**/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('public/css'));
 });
@@ -26,23 +27,23 @@ gulp.task('js', () => {
 
 });
 
-gulp.task("watch", () => {
-    gulp.watch('./assets/scss/*.scss', ['sass','css']);
-    gulp.watch('./assets/js/*.js', ['js']);
+gulp.task('watch', () => {
+    gulp.watch('assets/scss/*.scss', ['sass','css']);
+    gulp.watch('assets/js/*.js', ['js']);
 })
 
-gulp.task("serve", () => {
+gulp.task('serve', () => {
     return nodemon({
         script: 'server/index.js',
         env: {
-            NODE_ENV: 'development'
+            'NODE_ENV': 'development',
         }
     });
 });
 
-gulp.task("browser-sync", () => {
+gulp.task('browser-sync', () => {
     browserSync.init({
-        proxy: "localhost:8080/"
+        proxy: 'localhost:8080/'
     })
 });
 gulp.task('default', ['sass', 'css', 'watch', 'serve','browser-sync']);
