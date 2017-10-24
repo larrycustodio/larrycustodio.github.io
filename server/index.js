d08ba459802b4bf22bd5b52260c101d0e8c5fd26
Larry Custodio <custodioly@gmail.com>
2017-10-23 17:50:15 -0700

    added metadata

--- server/index.js
+++ server/index.js
@@ -13,9 +13,33 @@ app.use(express.static('public'))
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+const data = {
+    currentYear: new Date().getFullYear().toString(),
+    work: [
+        {
+            title: 'portfolio website',
+            link: 'https://larrycustod.io',
+            linkTag: 'you\'re here!',
+            img: 'img/work__screenshot-0.png',
+            desc: 'Designed using ea',
+            stack: ['sass','express','node','travis','nginx']
+        },
+        {
+            title: 'breakfast finder',
+            link: 'https://github.com/larrycustodio/breakfast-finder',
+            linkTag: 'GitHub Link',
+            img: 'img/work__screenshot-1.png',
+            desc: 'Single page app that displays nearby breakfast spots based on preference and location. ' + 
+            'Designed graphics and UI using Adobe Illustrator, '+ 
+            'used React, Node.js, and Express for client and server-side rendering' + 
+            'and Yelp and Google Maps API to fetch data.',
+            stack: ['express','react','node','travis']
+        },
+    ]
+}
+
 app.get('/', (req,res) => {
-    const currentYear = new Date().getFullYear().toString();
-    res.render('index',{year: currentYear});
+    res.render('index', data);
 });
 
 app.listen(PORT,()=>{
