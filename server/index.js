b04cc300af26b7dd2c9dd2709e952789997be922
Larry Custodio <custodioly@gmail.com>
2017-10-23 20:50:49 -0700

    modified work to display cards

--- server/index.js
+++ server/index.js
@@ -15,23 +15,23 @@ app.set('view engine', 'ejs');
 
 const data = {
     currentYear: new Date().getFullYear().toString(),
-    work: [
+    workHighlights: [
         {
-            title: 'portfolio website',
+            title: 'Portfolio Website',
             link: 'https://larrycustod.io',
             linkTag: 'you\'re here!',
             img: 'img/work__screenshot-0.png',
-            desc: 'Designed using ea',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam sit amet quam id laoreet. Maecenas ac aliquet risus. Mauris ac metus efficitur, fermentum massa non, ultricies massa. Proin nec massa eget erat hendrerit luctus nec et sapien. Donec venenatis, turpis eu porttitor cursus, nunc leo consectetur ',
             stack: ['sass','express','node','travis','nginx']
         },
         {
-            title: 'breakfast finder',
+            title: 'Breakfast Finder',
             link: 'https://github.com/larrycustodio/breakfast-finder',
             linkTag: 'GitHub Link',
             img: 'img/work__screenshot-1.png',
             desc: 'Single page app that displays nearby breakfast spots based on preference and location. ' + 
             'Designed graphics and UI using Adobe Illustrator, '+ 
-            'used React, Node.js, and Express for client and server-side rendering' + 
+            'used React, Node.js, and Express for client and server-side rendering, ' + 
             'and Yelp and Google Maps API to fetch data.',
             stack: ['express','react','node','travis']
         },
