0cc2da4530e6a456c92f18a33d876a071360a58c
Larry Custodio <custodioly@gmail.com>
2018-03-24 12:18:21 -0700

    Pruned gulp, migrating setup in webpack 4 WIP

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
 const app = express();
-const PORT = 8080;
-const axios = require('axios');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.static('dist'));
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
@@ -25,19 +25,9 @@ app.get('/about', (req,res) => {
 
 app.get('/blog', (req,res)=>{
     res.redirect('http://bitly.com/98K8eH');
-})
-
-app.post('/contact', (req, res) => {
-    axios.post('https://formspree.io/hello@larrycustod.io',
-    req.body)
-    .then(success => {
-        res.send(res.status);
-    })
-    .catch(err => {
-        res.send(res.status);
-    });
 });
 
-app.listen(PORT, () => {
-    console.log('Listening on localhost:' + PORT);
-})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on http://127.0.0.1:${process.env.PORT}/!`);
+});
