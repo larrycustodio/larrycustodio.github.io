b22b0ecbf1d50d0572f8c3426ca9c4f247bae2d1
Larry Custodio <custodioly@gmail.com>
2018-04-03 13:57:22 -0700

    base file builders added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const plumber = require('gulp-plumber');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
 
-gulp.task('sass', () =>
+gulp.task('build-html', () =>
+    gulp.src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('compile-sass', () =>
     gulp.src('src/scss/main.scss')
         .pipe(plumber())
         .pipe(sass())
@@ -12,7 +23,7 @@ gulp.task('sass', () =>
         .pipe(gulp.dest('src/css'))
 );
 
-gulp.task('css', () =>
+gulp.task('build-css', () =>
     gulp.src('src/css/main.css')
         .pipe(plumber())
         .pipe(sourcemaps.init())
@@ -22,14 +33,26 @@ gulp.task('css', () =>
         .pipe(gulp.dest('dist'))
 );
 
-gulp.task('js', () =>
+gulp.task('build-js', () =>
     gulp.src('src/js/*.js')
+        .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(concat('app.min.js'))
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(concat('app.concat.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('app.concat.min.js'))
+        .pipe(uglify())
         .pipe(sourcemaps.write())
+        .pipe(plumber.stop())
         .pipe(gulp.dest('dist'))
 );
 
-gulp.task('cssify', ['sass','css']);
+gulp.task('default', ['build-html', 'compile-sass', 'build-css', 'build-js']);
 
-gulp.task('default', ['sass', 'css', 'js']);
+gulp.task('watch', ()=>{
+    gulp.watch('src/**/*.html', ['build-html']),
+    gulp.watch('src/scss/**/*.scss', ['compile-sass', 'build-css']),
+    gulp.watch('src/js/**/*.js', ['build-js'])
+});
