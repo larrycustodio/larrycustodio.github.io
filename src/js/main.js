90dabcaacdd876e5df8faad1d43aeb76d074766a
Larry Custodio <custodioly@gmail.com>
2018-04-03 21:35:06 -0700

    added class toggles for project container

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,17 @@
 class ProjectsContainer {
     constructor() {
         this.projects = document.querySelectorAll('.project__container');
+        this.scrollWindow = 0;
+        this.ticking = false;
+        this.scrollHandler = this.scrollHandler.bind(this);
     }
+
     addContainerBorders() {
+        // Create individual sides positioned absolute as div elements
+        // Then add as child node to each project container 
         if (!!this.projects.length) {
             [...this.projects].forEach(project => {
-                [   
+                [
                     { position: 'top', coord: 'x' },
                     { position: 'bottom', coord: 'x' },
                     { position: 'left', coord: 'y' },
@@ -19,8 +25,38 @@ class ProjectsContainer {
             })
         }
     }
+
+    addScrollModifiers() {
+        // Add scroll listeners for toggling active class on project containers  
+        window.addEventListener('scroll', this.scrollHandler, false);
+    }
+
+    scrollHandler() {
+        // Set scrollWindow based on window scroll position
+        // Check if scrollWindow is either
+        this.scrollWindow = window.scrollY;
+        [...this.projects].forEach(project => {
+            // Set bounds for changing the container classes
+            // Once it's within conditional bounds, toggle .container--active on container
+            // and toggle border--active on container
+            const isContainerInView = this.scrollWindow >= (project.offsetTop * 0.5);
+            if(isContainerInView){
+                project.classList.add('container--active');
+            }
+        });
+        // if (!this.ticking) {
+        //     requestAnimationFrame(this.updateContainerClass);
+        // }
+        this.ticking = true;
+    }
+
+    updateContainerClass(){
+        console.log(this.scrollY);
+    }
+
     init() {
         this.addContainerBorders();
+        this.addScrollModifiers();
     }
 };
 
